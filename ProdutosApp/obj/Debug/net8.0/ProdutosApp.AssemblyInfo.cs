dd4f62300d33d69a85593bd6e812dae2b8fa54a6
cotiaws <cotiaws5@gmail.com>
2025-03-22 18:00:54 -0300

    Mapeamento do EF, Migrations, Repositórios e API de consulta de categorias

--- ProdutosApp/obj/Debug/net8.0/ProdutosApp.AssemblyInfo.cs
+++ ProdutosApp/obj/Debug/net8.0/ProdutosApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProdutosApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d736a0680a2ae0ec8859ed2c7d79e51d70be790")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProdutosApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProdutosApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
