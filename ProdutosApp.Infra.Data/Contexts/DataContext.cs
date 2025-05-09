73c06046f2baf21f19a175e75fe8c88f1aef3ed3
cotiaws <cotiaws5@gmail.com>
2025-05-08 21:20:54 -0300

    fix: Ajustes no processo da PIPELINE

---
+++ ProdutosApp.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosApp.Infra.Data.Contexts
+{
+    internal class DataContext
+    {
+    }
+}
